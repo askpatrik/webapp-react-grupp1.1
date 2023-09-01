9af4be8aa740b77f351cbcc45536f1b49886e1c9
askpatrik <ask.patrik@hotmail.com>
2023-09-01 10:01:58 +0200

    Adjusted select component so that the topics are displayed outside of drop down menu

--- reactapp/src/Pages/Articles/components/select.jsx
+++ reactapp/src/Pages/Articles/components/select.jsx
@@ -7,8 +7,7 @@ const SelectsComponent = () => {
     const dispatch = useDispatch();
     const topics = ["All", "Politik", "Utbildning", "Religion", "Miljo", "Ekonomi", "LivsstilFritt", "SamhalleKonflikter", "Halsa", "Idrott", "VetenskapTeknik"]
 
-    const handleTopicChange = (e) => {
-        const selectedTopic = e.target.value;
+    const handleTopicChange = (selectedTopic) => {
         dispatch(setTopic(selectedTopic));
     };
 
@@ -19,17 +18,27 @@ const SelectsComponent = () => {
 
     return (
         <div>
-            <label htmlFor="topic">Select a topic:</label>
-            <select id="topic" name="topic" value={searchTopic} onChange={handleTopicChange}>
-                {
-                    topics.sort().map((t, i) => <option value={t} key={i}>{t}</option>)
-                }
-            </select>
+            <div>
+                <label htmlFor="topic">Select a topic:</label>
+                <div>
+                    {
+                        topics.sort().map((t, i) => (
+                            <button
+                                key={i}
+                                className={searchTopic === t ? 'selected' : ''}
+                                onClick={() => handleTopicChange(t)}
+                            >
+                                {t}
+                            </button>
+                        ))
+                    }
+                </div>
+            </div>
 
             <label htmlFor="sorting">Sort:</label>
             <select id="sorting" name="sorting" value={sorting} onChange={handleSortingChange}>
-                <option value="newest" selected>Newest</option>
-                <option value="oldest" selected>Oldest</option>
+                <option value="newest">Newest</option>
+                <option value="oldest">Oldest</option>
             </select>
         </div>
     );
